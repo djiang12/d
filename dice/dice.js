22a8024408c17cf2dc004a452a82b087d1a7f781
Jiang <djiang1@wpi.edu>
2023-07-13 10:26:15 -0400

    dice

---
+++ dice/dice.js
@@ -0,0 +1,11 @@
+function roll() {
+    const sides =6;
+   var randomnumber= Math.floor(Math.random() *sides)+1
+   let face = document.getElementById("face");
+   let img = document.getElementById("img");
+   face.innerHTML = randomnumber;
+   img.innerHTML= randomnumber;
+}
+var button = document.getElementById("button");
+button.addEventListener("click",roll);
+const images = ["place","https://www.random.org/dice/dice1.png","https://www.random.org/dice/dice2.png","https://www.random.org/dice/dice3.png","https://www.random.org/dice/dice4.png","https://www.random.org/dice/dice5.png","https://www.random.org/dice/dice6.png"];
